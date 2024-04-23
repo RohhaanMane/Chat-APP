af301ad01ea1dcd502ec7853751a411b3aeadc0a
D1-80706-RohanMane <rohhaanmane@gmail.com>
2024-04-24 00:27:39 +0530

    user route, controller added - to get users for sidebar

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
+import userRoutes from './routes/user.routes.js'
 import connectToMongodb from './db/connectToMongodb.js';
 
 const app = express();
@@ -16,6 +17,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // root route http://localhost:5000/
 // app.get("/", (req, res)=>{
