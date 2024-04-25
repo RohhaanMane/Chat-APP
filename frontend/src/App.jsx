9c9b2b020f42f9797ab72f0f13f80a16923a1a87
D1-80706-RohanMane <rohhaanmane@gmail.com>
2024-04-25 22:02:22 +0530

    Sign up UI done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Login from './login/Login'
+import SignUp from './signup/SignUp'
 
 function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Login />
+      {/* <Login /> */}
+      <SignUp />
     </div>
   )
 }
--- frontend/src/signup/SignUp.jsx
+++ frontend/src/signup/SignUp.jsx
@@ -1,7 +1,51 @@
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+      <h1 className='text-3xl font-semibold text-center text-gray-300'>
+					Sign Up <span className='text-blue-500'> ChatApp</span>
+			</h1>
+
+      <form>
+        <div>
+          <label className="label p-2">
+            <span className="label-text text-base">Full Name</span>
+          </label>
+          <input type="text" placeholder="Jhon Doe" className="w-full input input-bordered"/>
+        </div>
+
+        <div>
+          <label className="label p-2">
+            <span className="label-text text-base">Username</span>
+          </label>
+          <input type="text" placeholder="Jhondoe" className="w-full input input-bordered"/>
+        </div>
+
+        <div>
+          <label className="label p-2">
+            <span className="label-text text-base">Password</span>
+          </label>
+          <input type="password" placeholder="Enter password" className="w-full input input-bordered"/>
+        </div>
+
+        <div>
+          <label className="label p-2">
+            <span className="label-text text-base">Confirm Password</span>
+          </label>
+          <input type="password" placeholder="Confirm password" className="w-full input input-bordered"/>
+        </div>
+
+        {/* gender checkbox */}
+
+        <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+          Already have an account
+        </a>
+        <button className="btn btn-block btn-sm mt-2 border border-slate-700">Sign Up</button>
+      </form>
+
+      </div>
+    </div>
   )
 }
 
