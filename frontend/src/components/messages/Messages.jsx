43c56b1ad93b1fc38736fc17d53bf5109b00f09d
D1-80706-RohanMane <rohhaanmane@gmail.com>
2024-05-07 18:54:12 +0530

    got all senders and receivers messges and showd in chat

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -6,7 +6,22 @@ const Messages = () => {
     const { messages, loading } = useGetMessages();
     return (
         <div className="px-4 flex-1 overflow-auto">
-            <MessageSkeleton />
+            {/* if there are messages */}
+            {!loading &&
+                messages.length > 0 &&
+                messages.map((message, idx) => (
+                    <Message message={message} key={idx} />
+                ))}
+            {/* for message skeleton load */}
+            {loading &&
+                [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+
+            {/* After loading if there are no messages (no conversation between you and other user) */}
+            {!loading && messages.length === 0 && (
+                <p className="text-center mt-3">
+                    Send a message to start the conversation
+                </p>
+            )}
         </div>
     );
 };
