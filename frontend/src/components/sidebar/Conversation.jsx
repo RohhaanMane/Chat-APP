b3443ae9f22960ffb0c03cfad2dd30c014ea1f33
D1-80706-RohanMane <rohhaanmane@gmail.com>
2024-05-07 16:13:48 +0530

    get state from zustand and added feature of selecting a user to chat - css

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,7 +1,17 @@
+import useConversation from "../../zustand/useConversation";
 const Conversation = ({ conversation, emoji, lastIdx }) => {
+    const { selectedConversation, setSelectedConversation } = useConversation();
+
+    const isSelected = selectedConversation?._id === conversation._id;
+
     return (
         <>
-            <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+            <div
+                className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+                ${isSelected ? "bg-sky-500" : ""}
+            `}
+                onClick={() => setSelectedConversation(conversation)}
+            >
                 <div className="avatar online">
                     <div className="w-12 rounded-full">
                         <img src={conversation.profilePic} alt="user avatar" />
@@ -9,7 +19,9 @@ const Conversation = ({ conversation, emoji, lastIdx }) => {
                 </div>
                 <div className="flex flex-col flex-1">
                     <div className="flex gap-3 justify-between">
-                        <p className="font-bold text-gray-200">{conversation.fullName}</p>
+                        <p className="font-bold text-gray-200">
+                            {conversation.fullName}
+                        </p>
                         <span className="text-xl">{emoji}</span>
                     </div>
                 </div>
