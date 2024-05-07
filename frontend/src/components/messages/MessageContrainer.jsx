b710175cc11118eba95b8507a565c75bd40f597f
D1-80706-RohanMane <rohhaanmane@gmail.com>
2024-05-07 16:28:59 +0530

    first time login - welcome message - full name added

--- frontend/src/components/messages/MessageContrainer.jsx
+++ frontend/src/components/messages/MessageContrainer.jsx
@@ -1,12 +1,22 @@
+import { useEffect } from "react";
+import useConversation from "../../zustand/useConversation";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
-import {TiMessages} from "react-icons/ti"
+import { TiMessages } from "react-icons/ti";
+import { useAuthContext } from "../../context/AuthContext";
 
 const MessageContrainer = () => {
-    const noChatSelected = true;
+    const { selectedConversation, setSelectedConversation } = useConversation();
+    const { authUser } = useAuthContext();
+
+    useEffect(() => {
+        // clean up function - will run when this component will unmount
+        return () => setSelectedConversation(null);
+    }, [setSelectedConversation]);
+
     return (
         <div className="md:min-w-[450px] flex flex-col">
-            {noChatSelected ? (
+            {!selectedConversation ? (
                 <NoChatSelected />
             ) : (
                 <>
@@ -14,7 +24,7 @@ const MessageContrainer = () => {
                     <div className="bg-slate-500 px-4 py-2 mb-2">
                         <span className="label-text">To: </span>
                         <span className="text-gray-900 font-bold">
-                            Jhon Doe
+                            {selectedConversation.fullName}
                         </span>
                     </div>
 
@@ -29,10 +39,11 @@ const MessageContrainer = () => {
 export default MessageContrainer;
 
 function NoChatSelected() {
+    const { authUser } = useAuthContext();
     return (
         <div className="flex items-center justify-center w-full h-full">
             <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-                <p>Welcome 👋 Jhon Dow ❄</p>
+                <p>Welcome 👋 {`${authUser.fullName}`} ❄</p>
                 <p>Select a chat to start messaging</p>
                 <TiMessages className="text-3xl md:text-6xl text-center" />
             </div>
