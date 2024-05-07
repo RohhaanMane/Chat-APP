87bcb4f2672acf1873bd0ac8b27b84aff815b426
D1-80706-RohanMane <rohhaanmane@gmail.com>
2024-05-07 18:36:53 +0530

    Message Skeleton added

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,11 +1,12 @@
 import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
 
 const Messages = () => {
     const { messages, loading } = useGetMessages();
     return (
         <div className="px-4 flex-1 overflow-auto">
-            
+            <MessageSkeleton />
         </div>
     );
 };
