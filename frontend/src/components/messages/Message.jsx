0b4ec30ab4a1693b7185a9b6caef8325c63f90e1
D1-80706-RohanMane <rohhaanmane@gmail.com>
2024-05-07 19:03:00 +0530

    Date extracting functionality added

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,29 +1,33 @@
-import { useAuthContext } from "../../context/AuthContext"
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
 import useConversation from "../../zustand/useConversation";
 
-const Message = ({message}) => {
-  const {authUser} = useAuthContext();
-  const {selectedConversation} = useConversation();
-  const fromMe = message.senderId === authUser._id;
-  const chatClassName = fromMe ? "chat-end": "chat-start";
-  const profilePic = fromMe ? authUser.profilePic : selectedConversation.profilePic;
-  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+const Message = ({ message }) => {
+    const { authUser } = useAuthContext();
+    const { selectedConversation } = useConversation();
+    const fromMe = message.senderId === authUser._id;
+    const formattedTime = extractTime(message.createdAt);
+    const chatClassName = fromMe ? "chat-end" : "chat-start";
+    const profilePic = fromMe
+        ? authUser.profilePic
+        : selectedConversation?.profilePic;
+    const bubbleBgColor = fromMe ? "bg-blue-500" : "";
 
-  return (
-    <div className={`chat ${chatClassName}` }>
-        <div className="chat-image avatar">
-            <div className="w-10 rounded-full">
-                <img src={profilePic} alt="Tailwind css chat bubble" />
+    return (
+        <div className={`chat ${chatClassName}`}>
+            <div className="chat-image avatar">
+                <div className="w-10 rounded-full">
+                    <img src={profilePic} alt="Tailwind css chat bubble" />
+                </div>
+            </div>
+            <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+                {message.message}
+            </div>
+            <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+                {formattedTime}
             </div>
         </div>
-        <div className={`chat-bubble text-white ${bubbleBgColor}`}>
-        {message.message}
-        </div>
-        <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
-            {message.createdAt}
-        </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Message
+export default Message;
